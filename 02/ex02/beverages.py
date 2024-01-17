b31f178e5fd06afb142f13bd43b628615b3fb0a2
JaemooJung <dbdbwer@naver.com>
2024-01-17 14:53:12 +0100

    add init to all classes

--- 02/ex02/beverages.py
+++ 02/ex02/beverages.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
 class HotBeverage:
-    name = "hot beverage"
-    price = 0.30
-    _description = "Just some hot water in a cup."
+    name: str
+    price: float
+    _description: str
+
+    def __init__(self, name="hot beverage", 
+                 price=0.30, 
+                 description="Just some hot water in a cup."):
+        self.name = name
+        self.price = price
+        self._description = description
 
     def description(self):
         return self._description
@@ -13,22 +20,20 @@ class HotBeverage:
 
 
 class Coffee(HotBeverage):
-    name = "coffee"
-    price = 0.40
-    _description = "A coffee, to stay awake."
+    def __init__(self):
+        super().__init__("coffee", 0.40, "A coffee, to stay awake.")
 
 
 class Tea(HotBeverage):
-    name = "tea"
+    def __init__(self):
+        super().__init__("tea")
 
 
 class Chocolate(HotBeverage):
-    name = "chocolate"
-    price = 0.50
-    _description = "Chocolate, sweet chocolate..."
+    def __init__(self):
+        super().__init__("chocolate", 0.50, "Chocolate, sweet chocolate...")
 
 
 class Cappuccino(HotBeverage):
-    name = "cappuccino"
-    price = 0.45
-    _description = "Un po’ di Italia nella sua tazza!"
+    def __init__(self):
+        super().__init__("cappuccino", 0.45, "Un po’ di Italia nella sua tazza!")
