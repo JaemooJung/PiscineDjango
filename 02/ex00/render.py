78ebab30b931835f3926787e087476d32b0838ad
JaemooJung <dbdbwer@naver.com>
2024-01-17 13:09:35 +0100

    d02-ex00 done

---
+++ 02/ex00/render.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import os
+import sys
+import re
+import settings
+
+def main():
+    if (len(sys.argv) != 2):
+        return print("wrong argument count")
+    path = sys.argv[1]
+    regex = re.compile(".*\.template")
+    if not regex.match(path):
+        return print("wrong extension, (required: .template)")
+    if not os.path.isfile(path):
+        return print("does not exit file... : {}".format(path))
+    with open(path, "r") as f:
+        template = "".join(f.readlines())
+    try:
+      file = template.format(
+          title=settings.title,
+          first_name=settings.first_name,
+          last_name=settings.last_name,
+          age=settings.age, 
+          profession=settings.profession,
+          mobile=settings.mobile,
+          email=settings.email
+      )
+    except Exception as e:
+      return print("Error: {}".format(e))
+    regex = re.compile("(\.template)")
+    path = "".join([path[0:regex.search(path).start()], ".html"])
+    with open(path, "w") as f:
+        f.write(file)
+
+if __name__ == '__main__':
+    main()
---
+++ 02/ex00/settings.py
@@ -0,0 +1,7 @@
+title = "Jaemoo Jung's Resume"
+first_name = "Jaemoo"
+last_name = "Jung"
+age = 27
+profession = "Software Engineer"
+mobile = "07 80 88 17 55"
+email = "Jaemoo0330@gmail.com"
