46d2f7a2c1835526c88a6faefe63c83775e06400
JaemooJung <jaemoo0330@gmail.com>
2024-01-19 14:01:44 +0100

    delete unrequired files for day00

--- 00/ex00/myawesomescript.sh
+++ 00/ex00/myawesomescript.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-# Check if the bit.ly URL is provided as a parameter
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <bit.ly URL>"
-  exit 1
+if [ $1 ]; then
+  curl -s -I "$1" | grep -i "Location" | cut -d' ' -f2-
 fi
-
-# Get the bit.ly URL from the first parameter
-url=$1
-
-# Use curl to fetch the headers of the URL and grep to extract the "Location" header
-location=$(curl -s -I "$url" | grep -i "Location" | cut -d' ' -f2-)
-
-# Display the real address
-echo $location
