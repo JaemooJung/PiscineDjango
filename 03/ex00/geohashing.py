38134c6619c719627d7d8d5d294267d043c44f63
JaemooJung <jaemoo0330@gmail.com>
2024-01-22 12:42:31 +0100

    d03-ex00 done

---
+++ 03/ex00/geohashing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import sys
+import antigravity
+
+def main():
+
+    usage_message = "Usage: python3 geohashing.py <date: string> <latitude: float> <longitude: float>"
+
+    if len(sys.argv) != 4:
+        print(usage_message)
+        return
+    try:
+        antigravity.geohash(float(sys.argv[2]), 
+                            float(sys.argv[3]), 
+                            bytes(sys.argv[1], 'utf-8'))
+    except Exception as e:
+        print(f'invalid input: {e}')
+        print(usage_message)
+
+if __name__ == '__main__':
+    main()
