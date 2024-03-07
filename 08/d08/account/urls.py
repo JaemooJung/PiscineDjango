fb55bf40244b44a3ae0e670deffb68b045981528
JaemooJung <jaemoo0330@gmail.com>
2024-03-07 12:38:34 +0100

    d08- add signup

--- 08/d08/account/urls.py
+++ 08/d08/account/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.AccountView.as_view(), name='account'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.do_login, name='login'),
     path('logout/', views.do_logout, name='logout'),
     path('check_login/', views.check_login, name='check_login'),
--- 08/d08/account/views.py
+++ 08/d08/account/views.py
@@ -1,10 +1,17 @@
+from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
 from django.views.decorators.http import require_POST
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.views import View
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 from django.middleware.csrf import get_token
+from django.urls import reverse_lazy
+from typing import Any
+from django.contrib import messages
+from django.shortcuts import redirect
 
 class AccountView(View):
     def get(self, request):
@@ -12,6 +19,18 @@ class AccountView(View):
             "login_form": AuthenticationForm(),
         }
         return render(request, 'account.html', context)
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('account')
+    template_name = 'signup.html'
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        if request.user.is_authenticated:
+            messages.error(request, "You are already logged in.")
+            return redirect('account')
+        return super().dispatch(request, *args, **kwargs)
+
 
 def check_login(request):
     if request.user.is_authenticated:
